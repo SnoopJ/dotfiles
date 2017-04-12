1c485f0ba3a4a661a99674070f72a88720f9fb32
James Gerity <snoopjedi@gmail.com>
2017-04-12 15:46:01 -0500

    Remove unnecessary update command

--- linkservices.sh
+++ linkservices.sh
@@ -6,8 +6,4 @@
 export DOTFILES_DIR EXTRA_DIR
 DOTFILES_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-# Update dotfiles itself first
-
-#[ -d "$DOTFILES_DIR/.git" ] && git --work-tree="$DOTFILES_DIR" --git-dir="$DOTFILES_DIR/.git" pull origin master
-
 sudo cp -as "$(pwd)/services/"* /etc/systemd/system/
