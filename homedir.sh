cce5b1c756004e4ca0977c271f5e4e6b2908165d
James Gerity <snoopjedi@gmail.com>
2018-02-08 22:43:22 +0000

    Ensure FETCH_HEAD exists when running install script

--- homedir.sh
+++ homedir.sh
@@ -5,6 +5,9 @@
 
 export DOTFILES_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
+# make sure FETCH_HEAD exists
+touch $DOTFILES_DIR/.git/FETCH_HEAD
+
 # Update dotfiles itself first
 #[ -d "$DOTFILES_DIR/.git" ] && git --work-tree="$DOTFILES_DIR" --git-dir="$DOTFILES_DIR/.git" pull origin master
 
