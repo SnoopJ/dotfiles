6c922f593dba7f3604763bbb489a910dee4c915a
James Gerity <snoopjedi@gmail.com>
2019-04-22 09:21:32 -0500

    Add typed positional argument examples

--- .vim/templates/argparse.py
+++ .vim/templates/argparse.py
@@ -1,6 +1,8 @@
 import argparse
 
 parser = argparse.ArgumentParser(description='A useful description of this script (might want to set this to __doc__)', formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+parser.add_argument('strarg', type=str, help='A string argument')
+parser.add_argument('numarg', type=float, help='A numerical argument')
 parser.add_argument('-x', metavar='nameofx', nargs='+', type=float, default=32*32*32, help='Helpful message about this argument')
 
 grp = parser.add_mutually_exclusive_group()
