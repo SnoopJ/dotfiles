30bdeecf8600659ea56fca5521455849e15ddc2a
James Gerity <snoopjedi@gmail.com>
2019-08-14 10:58:50 -0500

    Clean up argpase and main() templates

--- .vim/templates/argparse.py
+++ .vim/templates/argparse.py
@@ -3,10 +3,7 @@ import argparse
 parser = argparse.ArgumentParser(description='A useful description of this script (might want to set this to __doc__)', formatter_class=argparse.ArgumentDefaultsHelpFormatter)
 parser.add_argument('strarg', type=str, help='A string argument')
 parser.add_argument('numarg', type=float, help='A numerical argument')
+parser.add_argument('numarg', type=float, help='A required argument', required=True)
 parser.add_argument('-x', metavar='nameofx', nargs='+', type=float, default=32*32*32, help='Helpful message about this argument')
-
-grp = parser.add_mutually_exclusive_group()
-grp.add_argument('--option1', dest='destination', action='store_const', const='option1_const')
-grp.add_argument('--option2', dest='destination', action='store_const', const='option2_const')
 
 args = parser.parse_args()
