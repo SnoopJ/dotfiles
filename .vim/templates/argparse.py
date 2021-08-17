d0f4fee65c94efebfeeb42923087cec9bc12962b
James Gerity <snoopjedi@gmail.com>
2021-08-17 16:16:38 -0400

    Add __name__ clause to argparse template

--- .vim/templates/argparse.py
+++ .vim/templates/argparse.py
@@ -6,4 +6,8 @@ parser.add_argument('numarg', type=float, help='A numerical argument')
 parser.add_argument('numarg', type=float, help='A required argument', required=True)
 parser.add_argument('-x', metavar='nameofx', nargs='+', type=float, default=32*32*32, help='Helpful message about this argument')
 
-args = parser.parse_args()
+def main():
+    args = parser.parse_args()
+
+if __name__ == "__main__":
+    main()
