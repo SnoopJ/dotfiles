07078ca0ddbff9fe508d47cf662c2f1aa40f2832
James Gerity <snoopjedi@gmail.com>
2023-03-06 13:26:27 -0500

    Fix duplicated argument name

--- .vim/templates/argparse.py
+++ .vim/templates/argparse.py
@@ -3,7 +3,7 @@ import argparse
 parser = argparse.ArgumentParser(description='A useful description of this script (might want to set this to __doc__)', formatter_class=argparse.ArgumentDefaultsHelpFormatter)
 parser.add_argument('strarg', type=str, help='A string argument')
 parser.add_argument('numarg', type=float, help='A numerical argument')
-parser.add_argument('numarg', type=float, help='A required argument', required=True)
+parser.add_argument('reqarg', type=float, help='A required argument', required=True)
 parser.add_argument('-x', metavar='nameofx', nargs='+', type=float, default=32*32*32, help='Helpful message about this argument')
 
 def main():
